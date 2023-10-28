fd4585c506f53601ec44d9a9033bd3f20903cdd6
MichaelMenebraye <michael@dufuna.org>
2023-10-28 12:36:17 +0100

    JUnit & Spring task

--- src/main/java/com/dufuna/berlin/michaelmenebraye/tax/controller/TaxController.java
+++ src/main/java/com/dufuna/berlin/michaelmenebraye/tax/controller/TaxController.java
@@ -1,30 +1,10 @@
 package com.dufuna.berlin.michaelmenebraye.tax.controller;
-
-import com.dufuna.berlin.michaelmenebraye.tax.DTO.TaxRequest;
 import com.dufuna.berlin.michaelmenebraye.tax.service.TaxCalculatorImp;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@RestController
-@RequestMapping("/api/v1")
 public class TaxController {
-
-//    endPoint: http://localhost:8080/api/v1/calculateTax
-//    method: Post:
-//    sample body: {"income": 20000}
-
-    @PostMapping("/calculateTax")
-    @ResponseBody
-    public Map<String, Double> calculateTax(@RequestBody TaxRequest request) {
+    public static void main(String[] args) {
         TaxCalculatorImp calculator = new TaxCalculatorImp();
-        double taxOwed = calculator.getTaxOwed(request.getIncome());
-
-        // Create a map to hold the response
-        Map<String, Double> response = new HashMap<>();
-        response.put("taxOwed", taxOwed);
-
-        return response;
+        double taxOwed = calculator.getTaxOwed(50000);
+        System.out.println("Tax owed: " + taxOwed);
     }
 }
